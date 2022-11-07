53f895cd32547b35f228dc168c267482ccb2058e
Rob Lucas <rob576821@gmail.com>
2022-11-07 12:58:16 +0000

    Syncing

--- reporting.py
+++ reporting.py
@@ -4,11 +4,25 @@
 
 
 
-def daily_average(data, monitoring_station, pollutant):
-    """Your documentation goes here"""
-    
-    ## Your code goes here
+def daily_average(data, monitoring_station:str, pollutant:str) -> list:
+    """
+    This function returns a list/array with the daily averages for a particular pollutant and monitoring station.
+    """
+    #Validate monitoring_station and pollutant.
+    try:
+        if monitoring_station in ["Harlington", "Marlyebone Road", "N Kensington"] and pollutant in ["no", "pm10", "pm25"]:
+            fileName = f"Pollution-London {monitoring_station}.csv"
+            a = []
+            with open(f"./data/{fileName}") as f:
+                pass
+            print(a)
 
+        else:
+            raise Exception("Invalid arguments passed (either as monitoring station or pollutant")
+    except Exception as e:
+        print(str(e))
+        ## Your code goes here
+daily_average("", "Harlington", "pm10")
 def daily_median(data, monitoring_station, pollutant):
     """Your documentation goes here"""
     
