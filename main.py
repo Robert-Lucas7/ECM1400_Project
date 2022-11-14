f66a9594a2024d1a4b1cdb79549dd2829ef63a36
Rob Lucas <rob576821@gmail.com>
2022-11-14 22:35:34 +0000

    Added complete functionality to main_menu()

--- main.py
+++ main.py
@@ -4,8 +4,37 @@
 
 
 def main_menu():
-    """Your documentation goes here"""
-    # Your code goes here
+    """
+    Prints the options of the different modules and takes an input to choose which module is to be accessed.
+    """
+    #Printing the different options
+    print("R - Access the PR module")
+    print("I - Access the MI module")
+    print("M - Access the RM module")
+    print("A - Print the About text")
+    print("Q - Quit the application")
+
+    #Validating the input
+    invalidInput = True
+    while invalidInput:
+        inp = input("Select a valid option").upper()
+        match inp: #Should an if elif else be used?
+            case "R": #Pollution reporting
+                reporting_menu()
+            case "I":
+                intelligence_menu()
+            case "M":
+                monitoring_menu()
+            case "A":
+                about()
+            case "Q":
+                invalidInput = False
+                quit()
+
+    
+def reporting_menu():
+    """"""
+    
 
 def monitoring_menu():
     """Your documentation goes here"""
@@ -21,9 +50,9 @@ def about():
     # Your code goes here
 
 def quit():
-    """Your documentation goes here"""
+    """quit() will print a message saying that the program is being terminated and will not return a value."""
     # Your code goes here
-
+    print("This program is terminating")
 
 
 
