41e6683ddc5c8101a01a6cd6ffb4fb802f98f786
Rob Lucas <rob576821@gmail.com>
2022-12-04 16:02:55 +0000

    Completed find_cyan_pixels()

--- intelligence.py
+++ intelligence.py
@@ -4,6 +4,7 @@
 import numpy as np
 from matplotlib import pyplot as mat_plot
 
+
 def find_red_pixels(map_filename, upper_threshold = 100, lower_threshold = 50):
     """
     Returns and saves a 2D array of where the red pixels occur in an image.
@@ -25,11 +26,25 @@ def find_red_pixels(map_filename, upper_threshold = 100, lower_threshold = 50):
     mat_plot.imsave('./data/map-red-pixels.jpg', red_pixels, cmap='Greys')
     return red_pixels
 
-find_red_pixels('map.png')
-def find_cyan_pixels(*args,**kwargs):
-    """Your documentation goes here"""
-    # Your code goes here
-
+def find_cyan_pixels(map_filename, upper_threshold=100, lower_threshold=50):
+    """
+    Returns and saves a 2D array of where the cyan pixels occur in an image.
+    """
+    rgb_img = mat_plot.imread(f'./data/{map_filename}') * 255 # returns rgba [red, green, blue, aplha]
+    shape = rgb_img.shape
+    cyan_pixels = np.zeros((shape[0], shape[1]))# using 3 as saving as a jpg not png so doesn't need the alpha.
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            red =rgb_img[row,col][0]
+            green = rgb_img[row,col][1]
+            blue = rgb_img[row, col][2]
+            if red < lower_threshold and green > upper_threshold and blue > upper_threshold: #pixel is deemed to be red.
+                cyan_pixels[row, col] = 0
+            else:
+                cyan_pixels[row, col] = 1
+    mat_plot.imsave('./data/map-cyan-pixels.jpg', cyan_pixels, cmap='Greys')
+    return cyan_pixels
+find_cyan_pixels('map.png')
 
 def detect_connected_components(*args,**kwargs):
     """Your documentation goes here"""
