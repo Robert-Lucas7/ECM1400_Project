a6babe568df29d67d5db1ee7c9d72a480f0a2791
Rob Lucas <rob576821@gmail.com>
2022-12-06 15:01:27 +0000

    Completed detect_connected_components

--- intelligence.py
+++ intelligence.py
@@ -5,13 +5,14 @@ import numpy as np
 from matplotlib import pyplot as mat_plot
 
 class NumpyQueue():
-    def __init__(self, size):
-        self.queue = np.empty(size)
+    def __init__(self, size, type):
+        self.data_type = type
+        self.queue = np.empty(size, dtype=type)
         self.top_pointer = -1
         self.bottom_pointer = -1
 
     def Enqueue(self, item):
-        if isinstance(item, float):
+        if isinstance(item, self.data_type):
             if self.IsEmpty():
                 self.top_pointer = 0
                 self.bottom_pointer = 0
@@ -21,17 +22,18 @@ class NumpyQueue():
                 self.top_pointer = (self.top_pointer + 1) % self.queue.size
             self.queue[self.top_pointer] = item
         else:
-            raise Exception("Element to be added to the queue must be of type float.")
+            raise Exception("Element to be added to the queue must be of the specified type during intialisation.")
     def Dequeue(self):
-        item = self.queue[self.bottom_pointer]
         if self.IsEmpty():
             raise Exception("Cannot dequeue item: Queue is empty.")
         elif self.bottom_pointer == self.top_pointer:
+            item = self.queue[self.bottom_pointer]
             self.bottom_pointer = -1
             self.top_pointer = -1
+            return item
         else:
-            self.bottom_pointer = (self.bottom_pointer - 1) % self.queue.size
-        return item
+            self.bottom_pointer = (self.bottom_pointer + 1) % self.queue.size
+            return self.queue[self.bottom_pointer]
     def IsEmpty(self):
         if self.bottom_pointer == -1 and self.top_pointer == -1:
             return True
@@ -61,9 +63,11 @@ def find_red_pixels(map_filename, upper_threshold = 100, lower_threshold = 50):
             green = rgb_img[row,col][1]
             blue = rgb_img[row, col][2]
             if red > upper_threshold and green < lower_threshold and blue < lower_threshold: #pixel is deemed to be red.
-                red_pixels[row, col] = 0
+                red_pixels[row, col] = 0 #0 is white for matplotlib 'greys' colourmap
             else:
                 red_pixels[row, col] = 1
+    print(red_pixels)
+    print(red_pixels.shape)
     mat_plot.imsave('./data/map-red-pixels.jpg', red_pixels, cmap='Greys')
     return red_pixels
 
@@ -82,26 +86,81 @@ def find_cyan_pixels(map_filename, upper_threshold=100, lower_threshold=50):
             if red < lower_threshold and green > upper_threshold and blue > upper_threshold: #pixel is deemed to be red.
                 cyan_pixels[row, col] = 0
             else:
-                cyan_pixels[row, col] = 255
+                cyan_pixels[row, col] = 1
     mat_plot.imsave('./data/map-cyan-pixels.jpg', cyan_pixels, cmap='Greys')
     return cyan_pixels
-find_cyan_pixels('map.png')
+#find_cyan_pixels('map.png')
+
+def find_pixel_neighbours(pixel_location, img_shape):
+    #if statements to check if the current pixel is one of the edge pixels.
+    eight_neighbours = []
+    #print(f"Pixel location: {pixel_location}")
+    row = pixel_location[0]
+    col = pixel_location[1]
+    #Validating whether the pixel_location is within the image size boundaries.
+    if row < 0 or row >= img_shape[0] or col < 0 or col >= img_shape[1]:
+        raise Exception("Pixel location is outside the image.")
+
+    if col != 0 and row != 0:#If pixel not top left, add top-left diagonal pixel
+        eight_neighbours.append((row - 1, col - 1))
+    if row != 0:#If pixel not in first row, add neighbour pixel directly above it.
+        eight_neighbours.append((row - 1, col))
+    if row !=0 and col != img_shape[1] - 1:#If pixel not top right, add top-right diagonal pixel
+        eight_neighbours.append((row - 1, col + 1))
+    
+    if col != 0:# If pixel isn't to the left, add left neighbour pixel
+        eight_neighbours.append((row, col - 1))
+    if col != img_shape[1] - 1:# If pixel isn't to the right, add right neighbour pixel
+        eight_neighbours.append((row, col + 1))
+    
+    if row != img_shape[0] - 1 and col != 0:# If pixel isn't in the bottom left corner, add pixel diagonally bottom left.
+        eight_neighbours.append((row + 1, col - 1))
+    if row != img_shape[0] - 1:#If pixel isn't at the bottom, add the pixel below it
+        eight_neighbours.append((row + 1, col))
+    if row != img_shape[0] - 1 and col != img_shape[1] - 1: #If pixel isn't in the bottom right, add pixel diagonally bottom-right.
+        eight_neighbours.append((row + 1, col + 1))
+    return eight_neighbours
+
+
 
 def detect_connected_components(IMG):#2 pixels (p, q) are connected if q is in the set of N8(p)
     """
     Returns all 8-connected components in the image as a 2D array.
     """
+
+
     shape = IMG.shape
-    MARK = np.zeros(shape[0], shape[1])
-    queue = np.empty(shape[0] * shape[1])
+    MARK = np.zeros((shape[0], shape[1]))
+   
+    queue = NumpyQueue(shape[0] * shape[1], object)
     
-
-    for row in shape[0]:
-        for col in shape[1]:
-            if IMG[row, col] == 0 and MARK[row, col] == 0:# Shouldn't this be 255 for white? Look into fixing it when saving the image.
+    connected_component_number = 1
+    connected_component_number_and_size = []
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            if IMG[row, col] == 0 and MARK[row, col] == 0:# IMG[row, col] == 0 (if pixel is white - representing a pavement) - change to 1 for white
                 MARK[row, col] = 1
+                queue.Enqueue((row, col))
+                
+                connected_component_size = 0
+                while not queue.IsEmpty():
+                    connected_component_size += 1
+                    first_item = queue.Dequeue()
+                    eight_neighbours = find_pixel_neighbours(first_item, shape)
+                    for neighbour in eight_neighbours:
+                        if IMG[neighbour] == 0 and MARK[neighbour] == 0:
+                            MARK[neighbour] = 1
+                            queue.Enqueue(neighbour)
+                connected_component_number_and_size.append((connected_component_number, connected_component_size))
+                connected_component_number += 1
+    print(MARK)
+    queue.Show()
+    print(connected_component_number_and_size)
 
+detect_connected_components(find_red_pixels('map.png'))
 
+#detect_connected_components(red_pixel_map)
+#detect_connected_components(find_red_pixels('map.png'))
 
 def detect_connected_components_sorted(*args,**kwargs):
     """Your documentation goes here"""
