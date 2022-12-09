968a57156f822c5e96f9117125904c57a98e3b3e
Rob Lucas <rob576821@gmail.com>
2022-12-09 18:55:41 +0000

    Continued with  documentation for the intelligence module

--- intelligence.py
+++ intelligence.py
@@ -2,8 +2,9 @@
 # You should modify the functions below to match
 # the signatures determined by the project specification
 import numpy as np
+from numpy.typing import ArrayLike
 from matplotlib import pyplot as mat_plot
-from utils import insertion_sort
+
 class NumpyQueue():
     """
     Creates a circular queue that is implemented with a NumPy array.
@@ -91,13 +92,22 @@ class NumpyQueue():
             return True
         else:
             return False
-def validate_filename(filename : str):
+def validate_filename(filename : str) -> bool:
+    """
+    Validates a given filename to see if it is suitable to use.
+
+    Args:
+        filename (str): The filename to validate.
+
+    Returns:
+        bool: True = the file is valid to use, False = the file is NOT valid to use.
+    """
     if ['<', '>', ':', '"', "/", '\\', '|', '?', '*'] in filename:
         return False
     for c in filename:
         if int(c) >= 0 and int(c) <= 31: #if the ASCII value of the character is between 0 and 31, it is invalid
             return False
-    #Check if jpg, regex search/ match
+    #Check if jpg, regex search/ match?????
 def find_red_pixels(map_filename : str, upper_threshold : int = 100, lower_threshold : int = 50) -> np.ndarray:
     """
     Returns and saves a 2D array of where the red pixels occur in an image.
@@ -108,7 +118,7 @@ def find_red_pixels(map_filename : str, upper_threshold : int = 100, lower_thres
         lower_threshold (int, optional): The lower threshold of to decide if a pixel is a red colour (0 - 255). Defaults to 50.
 
     Returns:
-        ndarray: A 2D array that contains a 1 if a red pixel is at that location or a 0 otherwise.
+        ndarray: A 2D array that contains a 255 if a red pixel is at that location or a 0 otherwise.
     """
     try:
         if not validate_filename(map_filename):
@@ -119,7 +129,7 @@ def find_red_pixels(map_filename : str, upper_threshold : int = 100, lower_thres
             raise ValueError("The upper_threshold and/or lower_threshold arguments are not in the correct range (0 - 255)")
         
         rgb_img = mat_plot.imread(f'./data/{map_filename}') * 255 # Scales the image returned to have the colour values range from 0 to 255 (to avoid rounding errors)
-        red_pixels = np.zeros(rgb_img.shape[:2])# using 3 as saving as a jpg not png so doesn't need the alpha.
+        red_pixels = np.zeros(rgb_img.shape[:2])
         for row in range(rgb_img.shape[0]): #iterating over every element in the 2D array rgb_img
             for col in range(rgb_img.shape[1]):
                 red =rgb_img[row,col][0]
@@ -133,7 +143,7 @@ def find_red_pixels(map_filename : str, upper_threshold : int = 100, lower_thres
     except Exception as e:
         print(e)
 #find_red_pixels("map.png")
-def find_cyan_pixels(map_filename : str, upper_threshold : int = 100, lower_threshold : int = 50):
+def find_cyan_pixels(map_filename : str, upper_threshold : int = 100, lower_threshold : int = 50) -> np.ndarray:
     """
     Returns and saves a 2D array of where the red pixels occur in an image.
 
@@ -143,7 +153,7 @@ def find_cyan_pixels(map_filename : str, upper_threshold : int = 100, lower_thre
         lower_threshold (int, optional): The lower threshold of to decide if a pixel is a cyan colour (0 - 255). Defaults to 50.
 
     Returns:
-        ndarray: A 2D array that contains a 1 if a cyan pixel is at that location or a 0 otherwise.
+        ndarray: A 2D array that contains a 255 if a cyan pixel is at that location or a 0 otherwise.
     """
     try:
         if not validate_filename(map_filename):
@@ -167,10 +177,21 @@ def find_cyan_pixels(map_filename : str, upper_threshold : int = 100, lower_thre
         print(e)
 #find_cyan_pixels('map.png')
 
-def find_pixel_neighbours(pixel_location, img_shape):
-    #if statements to check if the current pixel is one of the edge pixels.
+def find_pixel_neighbours(pixel_location : tuple[int, int], img_shape : tuple[int, int]) -> list[tuple[int, int]]:
+    """
+    Returns the 8-adjacent neighbours of a pixel at the location defined (pixel_location) by checking if the pixel is at an edge of the image.
+
+    Args:
+        pixel_location (tuple[int, int]): The pixel location in the form (row, column).
+        img_shape (tuple[int, int]): The shape of the image in the form (rows, columns).
+
+    Raises:
+        Exception: The pixel_location argument is outside of the image.
+
+    Returns:
+        list[tuple[int, int]]: a list of the positions of the 8-adjacent neighbours of the pixel.
+    """
     eight_neighbours = []
-    #print(f"Pixel location: {pixel_location}")
     row = pixel_location[0]
     col = pixel_location[1]
     #Validating whether the pixel_location is within the image size boundaries.
@@ -197,39 +218,51 @@ def find_pixel_neighbours(pixel_location, img_shape):
         eight_neighbours.append((row + 1, col + 1))
     return eight_neighbours
 
-def detect_connected_components(IMG):#2 pixels (p, q) are connected if q is in the set of N8(p)
+def detect_connected_components(IMG : ArrayLike) -> np.ndarray:#2 pixels (p, q) are connected if q is in the set of N8(p)
     """
-    Returns all 8-connected components in the image as a 2D array.
+    Returns all 8-connected components in the image, defined in a 2D array, and saves the connected components and sizes to a text file.
+
+    Args:
+        IMG (ArrayLike): A binary image as a 2D array.
+
+    Returns:
+        ndarray: A 2D ndarray which has a number in each position that indicates which connected component it is associated with (excluding 0 as it indicates that there is no conencted component at that position).
     """
-    shape = IMG.shape
-    MARK = np.zeros((shape[0], shape[1]), dtype=int)
-   
-    queue = NumpyQueue(shape[0] * shape[1], object)
+    MARK = np.zeros(IMG.shape[:2], dtype=int) # The shape of MARK is the first two elements in the shape of the image as the RGB/ RGBA values are not needed.
+    queue = NumpyQueue(IMG.shape[0] * IMG.shape[1], object)
     cc_number = 1
-    for row in range(shape[0]):
-        for col in range(shape[1]):
-            if IMG[row, col] == 0 and MARK[row, col] == 0:# IMG[row, col] == 0 (if pixel is white - representing a pavement) - change to 1 for white
-                MARK[row, col] = cc_number #Modification
+    for row in range(IMG.shape[0]):
+        for col in range(IMG.shape[1]):
+            if IMG[row, col] == 255 and MARK[row, col] == 0:
+                MARK[row, col] = cc_number #Modification - so that you can find the size and number associated with the connected component.
                 queue.Enqueue((row, col))
-                
-                while not queue.IsEmpty(): #like a breadth first search
+                while not queue.IsEmpty(): 
                     first_item = queue.Dequeue()
-                    eight_neighbours = find_pixel_neighbours(first_item, shape)
+                    eight_neighbours = find_pixel_neighbours(first_item, IMG.shape)
                     for neighbour in eight_neighbours:
-                        if IMG[neighbour] == 0 and MARK[neighbour] == 0:
+                        if IMG[neighbour] == 255 and MARK[neighbour] == 0: #If both the neighbour is a pavement and has not been visited, mark it as visited and add it to the queue.
                             MARK[neighbour] = cc_number
                             queue.Enqueue(neighbour)
-                #connected_component_number_and_size.append((connected_component_number, connected_component_size))
-                cc_number += 1
+                cc_number += 1 #It will look for the next connected component so increment the number of the connected component by 1.
     connected_components = get_connected_components_from_MARK(MARK)
     save_connected_components_to_file(connected_components, 'cc-output-2a')
     return MARK
 
-def get_connected_components_from_MARK(MARK):
-    connected_component_dict = {}
-    for row in range(MARK.shape[0]):
-        for col in range(MARK.shape[1]):
 
+
+def get_connected_components_from_MARK(MARK : ArrayLike) -> list[tuple[int, int]]:
+    """
+    Returns the connected components from MARK as a list of tuples in the form (connected_component_number, connected_component_size).
+
+    Args:
+        MARK (ArrayLike): A 2D array which has a number in each position that indicates which connected component it is associated with (excluding 0 as it indicates that there is no conencted component at that position). 
+
+    Returns:
+        list[tuple[int, int]]: list of tuples that contain the connected component's number and size respectively.
+    """
+    connected_component_dict = {}
+    for row in range(MARK.shape[0]):#Iterates over the MARK array and if the number (excluding 0) is present in the dictionary then increment it's value by 1, otherwise add the number as the key to the dictionary.
+        for col in range(MARK.shape[1]):
             if MARK[row, col] != 0:
                 if MARK[row, col] not in connected_component_dict.keys():
                     connected_component_dict[MARK[row, col]] = 1
@@ -237,23 +270,28 @@ def get_connected_components_from_MARK(MARK):
                     connected_component_dict[MARK[row, col]] += 1
     return list(connected_component_dict.items())
 
-def save_connected_components_to_file(connected_components : list[tuple], filename : str): #connected_components in form: [(component_number, component_size), ...]
+def save_connected_components_to_file(connected_components : list[tuple[int, int]], filename : str) -> None:
+    """
+    Saves the list of tuples in the form (connected_component_number, connected_component_size) to a text file
+
+    Args:
+        connected_components (list[tuple[int, int]]): list of conencted components in the form [(connected_component_number, connected_component_size)].
+        filename (str): the name of the file to save the list of connected components in. 
+    """
+    #===================== VALIDATE FILENAME HERE =========================
+    #======================================================================
     with open(f'./data/{filename}.txt', 'w') as f:
         for number, size in connected_components:
             f.write(f"Connected Component {number}, number of pixels = {size}\n")
         f.write(f"Total number of connected components = {len(connected_components)}")
 
-def detect_connected_components_sorted(MARK): #Finds connected components from 2D array MARK and sorts them in decreasing order.
-    """"""
+def detect_connected_components_sorted(MARK : ArrayLike ) -> None: #Finds connected components from 2D array MARK and sorts them in decreasing order.
+    
     #======== VALIDATE INPUT ARRAY IS 2D ===================
     #=======================================================
-    #_, connected_component_numbers_and_sizes = get_connected_components(MARK)
-    #print(connected_component_numbers_and_sizes)
-    #Sort connected_component_sizes in decreasing order
+    
     connected_components = get_connected_components_from_MARK(MARK)
-    #print(connected_components)
-    #Sort connected components based on size
-    #sorted_component_sizes = insertion_sort([x[1] for x in connected_components])
+    
     values = connected_components
     for i in range(1, len(values)):
         found_location = False
