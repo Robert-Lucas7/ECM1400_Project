0c7b6ac051f85f3561137e5922b0a991a2033740
Rob Lucas <rob576821@gmail.com>
2022-12-10 15:16:11 +0000

    Can get all monitoring sites and pollutants monitored at each location

--- monitoring.py
+++ monitoring.py
@@ -10,6 +10,7 @@
 import json
 import requests
 import datetime
+from matplotlib import pyplot as mat_plot
 def get_live_data_from_api(site_code='MY1',species_code='NO',start_date=None,end_date=None):
     """
     Return data from the LondonAir API using its AirQuality API. 
@@ -36,22 +37,46 @@ def get_live_data_from_api(site_code='MY1',species_code='NO',start_date=None,end
     res = requests.get(url)
     return res.json()
 
-#print(json.dumps(get_live_data_from_api(), indent=4))
-def get_data_when_refreshed(site_code='MY1',species_code='NO',start_date=None,end_date=None, ):
-    endpoint = "http://api.erg.ic.ac.uk/AirQuality/Data/SiteSpecies/SiteCode={SITECODE}/SpeciesCode={SPECIESCODE}/StartDate={STARTDATE}/EndDate={ENDDATE}/Period={PERIOD}/Units={UNITS}/Step={STEP}/Json"
-    url = endpoint.format(
-        SITECODE = site_code,
-        SPECIESCODE = species_code,
-        STARTDATE = start_date,
-        ENDDATE= end_date,
-        PERIOD = None,
-        UNITS = None,
-        STEP = None
-    )
-    res = requests.get(url)
-    print(res.json())
-    #print(json.dumps(res.json(), indent = 4))
-get_data_when_refreshed()
+def get_monitoring_site_and_species() -> dict:
+    """Returns a dictionary containing all of the site codes and pollutants monitored at each site.
+
+    Returns:
+        dict: dictionary containing all site codes as the keys. The values are a nested dictionary with keys of: "SiteName" and "Species" (key "Species" contains a list of pollutants monitored at that site)."""
+    endpoint = "http://api.erg.ic.ac.uk/AirQuality/Information/MonitoringSiteSpecies/GroupName=London/Json"
+    monitoring_site_details =  requests.get(endpoint).json()
+    site_codes_and_pollutants_monitored = {}
+    for site in monitoring_site_details["Sites"]["Site"]: #sites is a dictionary
+        site_codes_and_pollutants_monitored[site["@SiteCode"]] = {
+            "SiteName" : site["@SiteName"]
+        }
+        if type(site["Species"]) == list: # As site["Species"] is either a single dictionary or a list of dictionaries.
+            site_codes_and_pollutants_monitored[site["@SiteCode"]]["Species"] = [species["@SpeciesCode"] for species in site["Species"]]
+        else:
+            site_codes_and_pollutants_monitored[site["@SiteCode"]]["Species"] = [site["Species"]["@SpeciesCode"]]
+    return site_codes_and_pollutants_monitored
+
+def plot_pollutants_on_same_graph(start_date = None, end_date = None):
+    try:
+        pollutants_to_plot = []
+        inp = ''
+        while len(pollutants_to_plot) < 3 and inp.upper() != 'Q':
+            inp = input("Enter a pollutant from NO, PM10, or PM25 (enter Q to stop).\n")
+            if inp.upper() in ["NO", "PM10", "PM25"]:
+                if inp.upper() in pollutants_to_plot:
+                    print("Pollutant is already going to be plotted.")
+                else:
+                    pollutants_to_plot.append(inp.upper())
+            elif inp.upper() != "Q":
+                print("Enter a valid pollutant. (NO, PM10, or )\n")
+        site_code = input("Enter a site code.")
+        data = {}
+        for pollutant in pollutants_to_plot:
+            response_data = get_live_data_from_api(site_code = site_code, species_code=pollutant)
+            print(json.dumps(response_data, indent = 4))
+        
+    except:
+        pass
+#plot_pollutants_on_same_graph()
 def rm_function_1(*args,**kwargs):
     """Your documentation goes here"""
     # Your code goes here
